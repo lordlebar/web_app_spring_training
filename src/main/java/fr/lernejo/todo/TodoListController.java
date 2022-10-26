119249e6d3ddfa7c50e65f12312cb85a212b66c4
Corentin Lebarilier <corentin.lebarilier@epita.fr>
2022-10-26 17:29:10 +0200

    part5

---
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -0,0 +1,23 @@
+package fr.lernejo.todo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todo")
+public class TodoListController {
+    private final List<Todo> todos = new ArrayList<>();
+
+    @PostMapping
+    void addToDo(@RequestBody Todo todo)
+    {
+        todos.add(todo);
+    }
+    @GetMapping
+    Collection<Todo> getTodos(){
+        return todos;
+    }
+}
